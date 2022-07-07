6f63a12c76b4a1d79edbfbbfc90e847a41d05ffe
Lucas Matheus <62152683+lucasgomes14@users.noreply.github.com>
2022-07-06 21:12:27 -0300

    chess

---
+++ chess-system/src/boardgame/Position.java
@@ -0,0 +1,44 @@
+package boardgame;
+
+public class Position {
+	
+	//ATTRIBUTES
+	private int row;
+	private int column;
+	
+	//CONSTRUCTOR
+	public Position() {
+		
+	}
+
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	//METHODS
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	//TOSTRING
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+	
+	
+	
+}
